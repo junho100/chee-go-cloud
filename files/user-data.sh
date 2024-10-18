2e7800b423da270446ca93845ee61ce9ac26bfc7
junho100 <bemodesty306@naver.com>
2024-10-19 00:37:04 +0900

    Feat: add codedeploy

--- files/user-data.sh
+++ files/user-data.sh
@@ -9,3 +9,15 @@ sudo yum install docker -y
 sudo systemctl start docker
 
 sudo usermod -a -G docker $(whoami)
+
+sudo yum install -y ruby
+
+sudo yum install -y wget
+
+cd /home/ec2-user
+
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+
+chmod +x ./install
+
+sudo ./install auto
--- iam.tf
+++ iam.tf
@@ -22,7 +22,8 @@ module "backend_instance_role" {
   create_instance_profile = true
 
   trusted_role_services = [
-    "ec2.amazonaws.com"
+    "ec2.amazonaws.com",
+    "codedeploy.amazonaws.com"
   ]
 
   custom_role_policy_arns = [
@@ -37,5 +38,40 @@ module "backend_instance_policy" {
   path        = "/"
   description = "backend-policy"
 
-  policy = file("./files/backend-policy.json")
+  policy = templatefile("./files/backend-policy.tftpl", {
+    s3_bucket_arn = "arn:aws:s3:::${format(module.naming.result, "backend-bucket")}",
+    ecr_repo_arn  = aws_ecr_repository.backend_ecr_repository.arn
+  })
+
+
+}
+
+module "codedeploy_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  role_name               = format(module.naming.result, "cd-role")
+  role_requires_mfa       = false
+  create_role             = true
+  create_instance_profile = false
+
+  trusted_role_services = [
+    "codedeploy.amazonaws.com",
+    "ec2.amazonaws.com"
+  ]
+
+  custom_role_policy_arns = [
+    module.codedeploy_policy.arn
+  ]
+}
+
+module "codedeploy_policy" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = format(module.naming.result, "cd-policy")
+  path        = "/"
+  description = "codedeploy-policy"
+
+  policy = templatefile("./files/codedeploy-policy.tftpl", {
+    s3_bucket_arn = "arn:aws:s3:::${format(module.naming.result, "backend-bucket")}"
+  })
 }
