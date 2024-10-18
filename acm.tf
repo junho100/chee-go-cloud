741bca5e946ee645995af4e1fa43603efec86067
junho100 <bemodesty306@naver.com>
2024-10-18 19:35:28 +0900

    Feat: add server instances

--- acm.tf
+++ acm.tf
@@ -17,3 +17,19 @@ module "acm" {
 
   wait_for_validation = true
 }
+
+module "acm_ap_northeast_2" {
+  source = "terraform-aws-modules/acm/aws"
+
+  domain_name = var.domain_name
+  zone_id     = data.aws_route53_zone.target_host_zone.zone_id
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${var.domain_name}",
+    "${var.domain_name}",
+  ]
+
+  wait_for_validation = true
+}
--- iam.tf
+++ iam.tf
@@ -12,3 +12,30 @@ resource "aws_iam_user_policy" "web_cicd_iam_policy" {
   user   = aws_iam_user.web_cicd_iam_user.name
   policy = file("./files/web-cicd-policy.json")
 }
+
+module "backend_instance_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  role_name               = format(module.naming.result, "backend-role")
+  role_requires_mfa       = false
+  create_role             = true
+  create_instance_profile = true
+
+  trusted_role_services = [
+    "ec2.amazonaws.com"
+  ]
+
+  custom_role_policy_arns = [
+    module.backend_instance_policy.arn
+  ]
+}
+
+module "backend_instance_policy" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = format(module.naming.result, "backend-policy")
+  path        = "/"
+  description = "backend-policy"
+
+  policy = file("./files/backend-policy.json")
+}
