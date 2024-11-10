8f4df15ce5a30c31417516ce5f592565d142c632
junho100 <bemodesty306@naver.com>
2024-11-10 23:33:58 +0900

    Feat: add resources for cron job

--- code-deploy.tf
+++ code-deploy.tf
@@ -20,3 +20,25 @@ resource "aws_codedeploy_deployment_group" "backend_codedeploy_deployment_group"
   }
 }
 
+resource "aws_codedeploy_app" "noti_codedeploy_app" {
+  name = format(module.naming.result, "noti-cd-app")
+}
+
+resource "aws_codedeploy_deployment_group" "noti_codedeploy_deployment_group" {
+  app_name               = aws_codedeploy_app.noti_codedeploy_app.name
+  deployment_group_name  = format(module.naming.result, "noti-cd-deployment-group")
+  service_role_arn       = module.codedeploy_role.iam_role_arn
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = module.backend.tags_all.Name
+    }
+  }
+}
+
--- ecr.tf
+++ ecr.tf
@@ -3,3 +3,9 @@ resource "aws_ecr_repository" "backend_ecr_repository" {
 
   force_delete = true
 }
+
+resource "aws_ecr_repository" "noti_ecr_repository" {
+  name = format(module.naming.result, "noti-ecr")
+
+  force_delete = true
+}
